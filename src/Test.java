92857b0f013f324c4d5383a699dd8a4b3736c221
Soumya S <soumya.prathyu@gmail.com>
2024-10-22 20:29:27 +0530

    new page

---
+++ src/Test.java
@@ -0,0 +1,16 @@
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		display();
+		
+	}
+
+	
+	public static void display()
+	{
+		String name ="Soumya";
+		System.out.println(name);
+	}
+}
